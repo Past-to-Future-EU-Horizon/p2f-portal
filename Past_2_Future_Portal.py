84d8cd97cf29f87658af01c6d630e7ac018cf038
Garrett Speed <g.t.speed@uu.nl>
2025-09-05 17:19:05 +0200

    Add page configuration

---
+++ Past_2_Future_Portal.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Welcome to the Past 2 Future Data Portal")
+
+welcome_markdown = """
+This portal is for the Past 2 Future project, 
+grouping together source datasets that will go into our project, 
+and the datasets that are produced by the project. 
+"""
+
+st.markdown(welcome_markdown)
