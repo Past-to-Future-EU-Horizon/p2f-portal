08d5b215ea126636fd439a2794f47595efe3da79
Garrett Speed <g.t.speed@uu.nl>
2025-09-15 16:41:01 +0200

    Adding Content and Buttons to Front Page of Dashboard

--- p2f-portal/Past_2_Future_Portal.py
+++ p2f-portal/Past_2_Future_Portal.py
@@ -19,3 +19,35 @@ and the datasets that are produced by the project.
 """
 
 st.markdown(welcome_markdown)
+
+abstract_markdown = """
+**Project Abstract**
+
+Understanding past climate evolution is essential for improving 
+predictions of future climate change. Current Earth system models (ESMs) 
+rely on 170 years of stable data, but it is necessary to account for 
+longer-term changes and critical transitions that impact ecosystems and 
+societies. Enhancing our understanding of these processes is crucial 
+for making accurate forecasts. In this context, the EU-funded P2F project 
+will improve our understanding of the climatic and societal impacts of the 
+ongoing climate crisis by integrating paleoenvironmental data, ESMs and 
+theoretical approaches. The project will enhance ESM capabilities and 
+advance the Climate Model Intercomparison Project model development cycle 
+by providing paleo-informed models for future projections and developing 
+plausible climate change scenarios based on past events."""
+
+st.markdown(abstract_markdown)
+
+bg1_c1, bg1_c2, bg1_c3, bg1_c4 = st.columns(4)
+
+bg1_c1.link_button("Read more about the project on the EU Website 🇪🇺", 
+                   "https://doi.org/10.3030/101184070")
+
+if bg1_c2.button("Explore the datasets we're re-using"):
+    st.switch_page("pages/01_Source_Datasets.py")
+
+if bg1_c3.button("Explore the new datasets we've created"):
+    st.switch_page("pages/02_New_Datasets.py")
+
+if bg1_c4.button("Explore our project publications"):
+    st.switch_page("pages/03_Project_Publications.py")
