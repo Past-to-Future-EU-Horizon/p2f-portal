6387a768498c12fd64c7aba7c624c54f70acdd97
Garrett Speed <g.t.speed@uu.nl>
2025-10-23 12:55:36 +0200

    Add examples to source datasets and new datasets

--- p2f-portal/pages/01_Source_Datasets.py
+++ p2f-portal/pages/01_Source_Datasets.py
@@ -1,4 +1,5 @@
 from assets import disclosure_text
+import pandas as pd
 import streamlit as st
 
 st.set_page_config(layout="wide")
@@ -9,3 +10,25 @@ st.title("Explore Source Datasets")
 
 st.sidebar.image("./assets/EN_FundedbytheEU_RGB_POS.png")
 st.sidebar.text(disclosure_text.disclosure_text)
+
+edf = pd.read_excel("./assets/ExistingDatasets.xlsx")
+
+st.pills("Data Themes", options=["SSTs"])
+
+COLUMNS = 2
+
+dataset_column_dict = {}
+dfc = 0
+r = 0
+c = 0 
+for row in range(len(edf)%COLUMNS+1):
+    dataset_column_dict[r] = st.columns(COLUMNS)
+    for col in range(COLUMNS):
+        if dfc in edf.index:
+            dataset_column_dict[r][c].subheader(edf.loc[dfc].Title)
+            dataset_column_dict[r][c].text(edf.loc[dfc].Authors)
+            dataset_column_dict[r][c].link_button("Open Dataset", url=edf.loc[dfc].DOI)
+        dfc += 1
+        c += 1
+    r += 1
+    c = 0
