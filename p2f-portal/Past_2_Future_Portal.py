e2ba81f64b3d5edc9c04d897cb0f56d71a8cb66a
Garrett Speed <g.t.speed@uu.nl>
2026-09-16 11:43:53 +0200

    Security and version upgrades

--- p2f-portal/Past_2_Future_Portal.py
+++ p2f-portal/Past_2_Future_Portal.py
@@ -22,7 +22,7 @@ with st.sidebar.container(border=True):
                 link below:""")
     st.link_button(label="GitHub", url="https://github.com/Past-to-Future-EU-Horizon")
 
-st.sidebar.text("P2F Portal v0.0.47")
+st.sidebar.text("P2F Portal v0.0.48")
 
 welcome_markdown = """
 This portal is for the Past 2 Future project, 
